c9c4960fb90ce5406d3fa58af5c7fd30a41b6980
Abhinav0406 <indiralaabhinavchary@gmail.com>
2024-12-24 10:50:24 +0530

    add javascript

---
+++ server.js
@@ -0,0 +1,14 @@
+// Function to add two numbers
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Variables to hold the numbers
+let number1 = 5;
+let number2 = 10;
+
+// Calling the function and storing the result
+let result = addNumbers(number1, number2);
+
+// Displaying the result
+console.log("The sum of " + number1 + " and " + number2 + " is: " + result);
